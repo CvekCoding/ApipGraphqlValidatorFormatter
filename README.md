f5f1bd0fed97b3bc32da95fff954148b86088bd4
Sergey Logachev <serg@baitek.org>
2020-04-16 17:40:17 +0300

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Graphql error formatter for Api Platform
+Serialize validation errors to json in graphql requests.
---
+++ src/ValidateStage.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * This file is part of the Aqua Delivery package.
+ *
+ * (c) Sergey Logachev <svlogachev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Cvek\ApipGraphqlValidatorFormatter;
+
+use ApiPlatform\Core\GraphQl\Resolver\Stage\ValidateStageInterface;
+use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
+use ApiPlatform\Core\Bridge\Symfony\Validator\Exception\ValidationException;
+use ApiPlatform\Core\Validator\ValidatorInterface;
+use GraphQL\Error\Error;
+use GraphQL\Type\Definition\ResolveInfo;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class ValidateStage implements ValidateStageInterface
+{
+    private ValidateStageInterface $validateStage;
+    private ResourceMetadataFactoryInterface $resourceMetadataFactory;
+    private ValidatorInterface $validator;
+    private SerializerInterface $serializer;
+
+    public function __construct(ValidateStageInterface $validateStage,
+                                ResourceMetadataFactoryInterface $resourceMetadataFactory,
+                                ValidatorInterface $validator,
+                                SerializerInterface $serializer)
+    {
+        $this->validateStage = $validateStage;
+        $this->resourceMetadataFactory = $resourceMetadataFactory;
+        $this->validator = $validator;
+        $this->serializer = $serializer;
+    }
+
+    public function __invoke($object, string $resourceClass, string $operationName, array $context): void
+    {
+        try {
+            ($this->validateStage)($object, $resourceClass, $operationName, $context);
+        } catch (Error $error) {
+            $resourceMetadata = $this->resourceMetadataFactory->create($resourceClass);
+            $validationGroups = $resourceMetadata->getGraphqlAttribute($operationName, 'validation_groups', null, true);
+            try {
+                $this->validator->validate($object, ['groups' => $validationGroups]);
+            } catch (ValidationException $e) {
+                /** @var ResolveInfo $info */
+                $info = $context['info'];
+
+                throw Error::createLocatedError($this->serializer->serialize($e->getConstraintViolationList(),'json'), $info->fieldNodes, $info->path);
+            } catch (\Exception $e) {
+                throw $error;
+            }
+        }
+    }
+}
